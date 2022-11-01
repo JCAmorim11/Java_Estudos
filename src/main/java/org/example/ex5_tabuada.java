57cc48721664b6e640358c9a0ca000a0cb1b1679
Juliana Amorim <juliana.carol.amorim@gmail.com>
2022-10-31 22:59:07 -0300

    ex5 - tabuada

--- src/main/java/org/example/ex5_tabuada.java
+++ src/main/java/org/example/ex5_tabuada.java
@@ -1,9 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class ex5_tabuada {
 
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        int tabu;
+        System.out.println("Tabuada: ");
+        tabu = scan.nextInt();
+        for(int i=1; i<11;i++){
+            System.out.println(tabu+" X "+i+"= "+(i*tabu));
+        }
     }
 
 }
